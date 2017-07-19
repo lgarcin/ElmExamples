d17958548e4f53ce40519088bc7459de9c368eba
lgarcin <laurent.garcin@gmail.com>
2017-07-19 18:23:06 +0200

    Decomp

--- Decomp.elm
+++ Decomp.elm
@@ -1,27 +1,57 @@
 module Decomp exposing (..)
 
+import Html exposing (Html, text)
 
-import Html exposing (Html,text)
 
-decomp: Int -> List Int
-decomp n =
-  case n of
-    0 -> []
-    _ -> (n%2)::decomp(n//2)
+decomp : Int -> Int -> List Int
+decomp n b =
+    case n of
+        0 ->
+            []
 
-pgcd: Int -> Int -> Int
+        _ ->
+            (n % b) :: decomp (n // b) b
+
+
+pgcd : Int -> Int -> Int
 pgcd a b =
-  case b of
-    0 -> a
-    _ -> pgcd b (a%b)
+    case b of
+        0 ->
+            abs (a)
 
-euclide: Int -> Int -> (Int, Int, Int)
+        _ ->
+            pgcd b (a % b)
+
+
+div : Int -> Int -> ( Int, Int )
+div a b =
+    let
+        r =
+            a % b
+    in
+        ( (a - r) // b, r )
+
+
+euclide : Int -> Int -> ( Int, Int, Int )
 euclide a b =
     case b of
-    0 -> 1
-    _ -> 2
+        0 ->
+            if a < 0 then
+                ( -a, -1, 0 )
+            else
+                ( a, 1, 0 )
+
+        _ ->
+            let
+                ( q, r ) =
+                    div a b
+
+                ( d, u, v ) =
+                    euclide b r
+            in
+                ( d, v, u - q * v )
 
 
 main : Html a
 main =
-  (decomp 1234, pgcd 15 27) |> toString |> text
+    ( decomp 1234 10, euclide 458 473 ) |> toString |> text
